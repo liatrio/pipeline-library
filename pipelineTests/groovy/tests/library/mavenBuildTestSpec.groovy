fefb970f39e8b7d907eab666a30d16daafbd9687
Benjamin Stein <ben@liatrio.com>
2018-04-18 17:09:20 -0400

    add specific call stack order tests

--- pipelineTests/groovy/tests/library/mavenBuildTestSpec.groovy
+++ pipelineTests/groovy/tests/library/mavenBuildTestSpec.groovy
@@ -15,6 +15,7 @@ class mavenBuildTestSpec extends PipelineSpockTestBase {
             c.call()
         })
         addEnvVar("GIT_URL", "https://git-server.com/project/repo.git")
+        addEnvVar("GIT_BRANCH", "LIA-23-some-feature")
         addEnvVar("SONAR_URL", "https://some-sonar-url.io")
 
         when:
@@ -23,11 +24,12 @@ class mavenBuildTestSpec extends PipelineSpockTestBase {
 
         then:
         printCallStack()
-        assert helper.callStack.findAll { call ->
-            call.methodName == "sh"
-        }.any {
-            it.toString().contains("mvn -s path/to/settings.xml -Dsonar.host.url=https://some-sonar-url.io clean install -B")
-        }
+        assert helper.callStack.get(0).toString().trim() == "mavenBuild.call()"
+        assert helper.callStack.get(1).toString().trim() == "mavenBuild.hipchatSend({color=GRAY, notify=true, v2enabled=true, message=Building LIA-23-some-feature from: <a href=https://git-server.com/project/repo/commits/null>https://git-server.com/project/repo.git</a>})"
+        assert helper.callStack.get(2).toString().trim() == "mavenBuild.configFile({fileId=artifactory, variable=MAVEN_SETTINGS})"
+        assert helper.callStack.get(3).toString().trim() == "mavenBuild.configFileProvider([null], groovy.lang.Closure)"
+        assert helper.callStack.get(4).toString().trim() == "mavenBuild.sh(mvn -s path/to/settings.xml -Dsonar.host.url=https://some-sonar-url.io clean install -B)"
+        assert helper.callStack.get(5).toString().trim() == "mavenBuild.hipchatSend({color=GRAY, notify=true, v2enabled=true, message=Success: Built <a href=the jenkinsUrl>war</a> with changelist<a href=the jenkinsUrl> the jenkinsUrl</a>})"
         assertJobStatusSuccess()
     }
     def "run default maven BuildAndDeploy"() {
@@ -53,16 +55,13 @@ class mavenBuildTestSpec extends PipelineSpockTestBase {
         script.call()
 
         then:
-        assert helper.callStack.findAll { call ->
-            call.methodName == "sh"
-        }.any {
-            it.toString().contains("mvn clean deploy -B -DartifactoryUsername=jenkinsCredUsername -DartifactoryPassword=jenkinsCredPassword)")
-        }
-        assert helper.callStack.findAll { call ->
-            call.methodName = "hipchatSend"
-        }.any {
-            it.toString().contains("{color=GRAY, notify=true, v2enabled=true, message=Building LIA-23-some-feature from: <a href=https://git-server.com/project/repo/commits/92b4d3435f39c6be>https://git-server.com/project/repo.git</a>}")
-        }
+        printCallStack()
+        assert helper.callStack.get(0).toString().trim() == "mavenBuildAndDeploy.call()"
+        assert helper.callStack.get(1).toString().trim() == "mavenBuildAndDeploy.hipchatSend({color=GRAY, notify=true, v2enabled=true, message=Building LIA-23-some-feature from: <a href=https://git-server.com/project/repo/commits/92b4d3435f39c6be>https://git-server.com/project/repo.git</a>})"
+        assert helper.callStack.get(2).toString().trim() == "mavenBuildAndDeploy.usernamePassword({credentialsId=Artifactory, usernameVariable=USERNAME, passwordVariable=PASSWORD})"
+        assert helper.callStack.get(3).toString().trim() == "mavenBuildAndDeploy.withCredentials([{credentialsId=Artifactory, usernameVariable=USERNAME, passwordVariable=PASSWORD}], groovy.lang.Closure)"
+        assert helper.callStack.get(4).toString().trim() == "mavenBuildAndDeploy.sh(mvn clean deploy -B -DartifactoryUsername=jenkinsCredUsername -DartifactoryPassword=jenkinsCredPassword)"
+        assert helper.callStack.get(5).toString().trim() == "mavenBuildAndDeploy.hipchatSend({color=GRAY, notify=true, v2enabled=true, message=Success: Built <a href=the jenkinsUrl>war</a> with changelist<a href=the jenkinsUrl> the jenkinsUrl</a>})"
         assertJobStatusSuccess()
     }
 }
