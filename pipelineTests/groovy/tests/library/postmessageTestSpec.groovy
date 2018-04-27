aa03507a55100bbef9a5f586904e7f50a5ce42f8
Benjamin Stein <ben@liatrio.com>
2018-04-27 11:22:48 -0400

    use correct call method in test

--- pipelineTests/groovy/tests/library/postmessageTestSpec.groovy
+++ pipelineTests/groovy/tests/library/postmessageTestSpec.groovy
@@ -20,15 +20,14 @@ class postMessageTestSpec extends PipelineSpockTestBase {
 
         then:
         printCallStack()
-        assert helper.callStack.get(0).toString().trim() == "slackSend.call({color=GREEN, message=Build was successful, team=liatrio-team})"
+        assert helper.callStack.get(0).toString().trim() == "postMessage.call({color=GREEN, message=Build was successful, team=liatrio-team})"
+        assert helper.callStack.get(1).toString().trim() == "postMessage.slackSend({channel=false, color=GREEN, message=Build was successful, teamDomain=liatrio-team, token=slack-token})"
         assertJobStatusSuccess()
 
     }
     def "hipchat message send" () {
         given:
             addEnvVar("chatClient", "hipchat")
-            helper.registerAllowedMethod("slackSend",[Map.class], null)
-            helper.registerAllowedMethod("mattermostSend",[Map.class], null)
             helper.registerAllowedMethod("hipchatSend",[Map.class], null)
             def params = ["color": "GREEN", "message": "Build was successful", "team": "liatrio-team"]
         when:
@@ -36,7 +35,8 @@ class postMessageTestSpec extends PipelineSpockTestBase {
             script.call(params)
         then:
             printCallStack()
-            assert helper.callStack.get(0).toString().trim() == "hipchatSend.call({color=GREEN, message=Build was successful, team=liatrio-team})"
+            assert helper.callStack.get(0).toString().trim() == "postMessage.call({color=GREEN, message=Build was successful, team=liatrio-team})"
+            assert helper.callStack.get(1).toString().trim() == "postMessage.hipchatSend({color=GREEN, notify=true, v2enabled=true, message=Build was successful, token=hipchat-token})"
             assertJobStatusSuccess()
     }
 }
--- vars/postMessage.groovy
+++ vars/postMessage.groovy
@@ -8,13 +8,16 @@ def call(Map params) {
     switch(env.chatClient) {
         case "slack":
             slackSend channel: room, color: color, message: params.message, teamDomain: team, token: 'slack-token'
+            break
         case "hipchat":
             def notify = true
             if( color != "RED" && color != "GREEN")
                 notify = false
             hipchatSend color: color, notify: notify, v2enabled: true, message: message, token: 'hipchat-token'
+            break
         case "mattermost":
             mattermostSend color: 'color', message: 'Message from Jenkins Pipeline', text: message, token: 'mattermost-token'
+            break
         // case "stride":
         //     echo "stride command"
         default:
