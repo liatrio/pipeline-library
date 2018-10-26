3c9f33040b2c427db7abe54ef40e99c9eec69106
JGuzman4 <jguzman23@mail.csuchico.edu>
2018-10-26 12:45:31 -0700

    trying to add extra variables

--- src/org/Slack/Slack.groovy
+++ src/org/Slack/Slack.groovy
@@ -12,6 +12,17 @@ class Slack {
   def sendBuildStart(body) {
     def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
 
+    def author = "";
+
+    def getGitAuthor = {
+      def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+      author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit}").trim()
+    }
+    def message = "";
+
+    def getLastCommitMessage = {
+      message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
+    }
     def payload = JsonOutput.toJson([
         text: "build has started!",
         channel: "${body.channel}",
