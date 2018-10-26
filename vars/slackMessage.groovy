1cbcb67f6987765103fd340612da76e7fd6d11a3
JGuzman4 <jguzman23@mail.csuchico.edu>
2018-10-26 12:08:36 -0700

    adding slack message payload

--- vars/slackMessage.groovy
+++ vars/slackMessage.groovy
@@ -1,15 +1,23 @@
 #!/usr/bin/env groovy
 import org.Slack.Slack
+import groovy.json.JsonOutput
 
 def call(body) {
 
-  slackSend channel: "${body.channel}", botUser: false, message: "\/greet"
-  //Slack slack = new Slack()
-  //if (body.event == "build-start"){
-  //  slack.sendBuildStart()
-  //}
-  //if (body.event == "build-complete"){
-  //  slack.sendBuildComplete()
-  //}
+
+    def slackURL = "${body.slackURL}"
+    def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
+
+    def payload = JsonOutput.toJson([
+        text: "success!",
+        channel: "${body.channel}",
+        username: "Jenkins",
+        icon_url: jenkinsIcon,
+        attachments: []
+    ])
+
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+
+
 }
 
