c0204226e7cffcf14ac8ddd685aef6a252561469
JGuzman4 <jguzman23@mail.csuchico.edu>
2018-10-26 18:28:53 -0700

    adding variables to build complete message

--- vars/slackMessage.groovy
+++ vars/slackMessage.groovy
@@ -23,7 +23,21 @@ def call(body) {
     sh "curl -X POST --data-urlencode \'payload=${payload}\' ${body.slackURL}"
   }
   else if ("${body.event}" == "build-complete"){
-    slack.sendBuildComplete()
+    def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+    def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit}").trim()
+    def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim() 
+
+    def payload = slack.sendBuildComplete([
+                    slackURL: "${body.slackURL}",
+                    jobName: "${env.JOB_NAME}",
+                    buildNumber: "${env.BUILD_NUMBER}",
+                    branch: "${env.BRANCH_NAME}",
+                    title_link: "${env.BUILD_URL}",
+                    author: "${author}",
+                    message: "${message}",
+                    channel: "${body.channel}"
+                  ])
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${body.slackURL}"
   }
 
 }
