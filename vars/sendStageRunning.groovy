4a6b7683a68a2f6d5b22a4eb0bff22b67fd730c9
JGuzman4 <jguzman23@mail.csuchico.edu>
2018-10-28 18:48:58 -0700

    trying to use httpRequest plugin

--- vars/sendStageRunning.groovy
+++ vars/sendStageRunning.groovy
@@ -12,7 +12,9 @@ def call(messages) {
     if ("${stageNames[i]}" == "${env.STAGE_NAME}"){
       def payload = slack.sendStageRunning("${env.SLACK_ROOM}", stageNames[i], messages[i+1].ts)
       //sh(returnStdout: true, script: "curl --silent -X POST -H 'Authorization: Bearer ${env.SLACK_TOKEN}' -H \"Content-Type: application/json\" --data \'${payload}\' ${env.SLACK_WEBHOOK_URL}/api/chat.update").trim() 
-      httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: payload, url: "${env.SLACK_WEBHOOK_URL}/api/chat.update", validResponseCodes: '200'
+        def response = httpRequest customHeaders: [
+          [ name: 'Authorization', value: "Bearer ${env.SLACK_TOKEN}" ]
+        ], contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: payload, url: "${env.SLACK_WEBHOOK_URL}/api/chat.update"
     }
   }
 
