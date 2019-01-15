f0dba9c8c5f185a800ce3fae3befde93e7f1b5ee
Benjamin Stein <ben@liatrio.com>
2019-01-15 15:37:02 -0600

    add library for building a maven repo in a maven container

---
+++ vars/buildMavenInContainer.groovy
@@ -0,0 +1,20 @@
+def call(Map mavenParams){
+	docker.image("maven:3.0.5").inside('') {
+		if (!mavenParams.goals) {
+			mavenParams.build = [:]
+            if(env.GIT_BRANCH.endsWith(primaryBranch)){
+                mavenParams.goals = "clean deploy"
+            }
+            else
+                mavenParams.goals = "clean install"
+		}
+		buildMaven(mavenParams)
+		pom = readMavenPom file: 'pom.xml'
+        def version = pom.version
+        env.VERSION = version
+        env.GROUP =  pom.groupId
+        env.ARTIFACT =  pom.artifactId
+        env.PACKAGING = pom.packaging
+        env.DOCKER_TAG = "${pom.version}-${env.BUILD_ID}"
+	}
+}
