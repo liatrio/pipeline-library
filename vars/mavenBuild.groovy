3eb86d8b119a2de3e0f7b691bc1d28f0e7587b40
Benjamin Stein <ben@liatrio.com>
2019-01-15 15:53:10 -0600

    make mavenBuild library more general

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,9 +1,15 @@
 #!/usr/bin/env groovy
 
-def call() {
-    hipchatSend color: 'GRAY', notify: true, v2enabled: true, message: "Building ${env.GIT_BRANCH} from: <a href=${env.GIT_URL - ".git"}/commits/${env.GIT_COMMIT}>${env.GIT_URL}</a>"
-    configFileProvider([configFile(fileId: 'artifactory', variable: 'MAVEN_SETTINGS')]) {
-        sh "mvn -s $MAVEN_SETTINGS -Dsonar.host.url=${env.SONAR_URL} clean install -B"
+def call(mavenParams) {
+
+    def goals = mavenParams.goals ? mavenParams.goals : 'clean install'
+    configFileProvider([configFile(fileId: 'standard', variable: 'MAVEN_SETTINGS')]){
+    	withCredentials([usernamePassword(credentialsId: 'artfactory', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+            def command = ""
+            if(mavenParams.dir)
+                command += "cd ${mavenParams.dir} && "
+            command += "mvn -B -s $MAVEN_SETTINGS ${goals} -Dartifactory.username=${env.USERNAME} -Dartifactory.password=${env.PASSWORD} -Dartifactory.baseUrl=${config.artifactory.url}/artifactory"
+            sh command
+        }
     }
-    hipchatSend color: 'GRAY', notify: true, v2enabled: true, message: "Success: Built <a href=${RUN_DISPLAY_URL}>war</a> with changelist<a href=${RUN_CHANGES_DISPLAY_URL}> ${RUN_CHANGES_DISPLAY_URL}</a>"
 }
