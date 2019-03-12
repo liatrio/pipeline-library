fdc5385adf6736d93e77ddf48dc6fcdd7a4a90eb
Benjamin Stein <ben@liatrio.com>
2019-03-12 15:46:55 -0700

    run steps in maven container

--- vars/mavenJxBuild.groovy
+++ vars/mavenJxBuild.groovy
@@ -3,18 +3,19 @@ def call(params) {
 
     def appVersion = params.get("version","")
 
-    if( params.version ) {
-        sh "mvn versions:set -DnewVersion=${appVersion}"
-    }
-    else {
-        def pom = readMavenPom file: 'pom.xml'
-        appVersion = pom.version
-        // set environment variables
-    }
-    sh "export VERSION=${appVersion}"
-    sh "mvn clean install"
-    sh "skaffold version"
+    container('maven') {
+        if (params.version) {
+            sh "mvn versions:set -DnewVersion=${appVersion}"
+        } else {
+            def pom = readMavenPom file: 'pom.xml'
+            appVersion = pom.version
+            // set environment variables
+        }
+        sh "export VERSION=${appVersion}"
+        sh "mvn clean install"
+        sh "skaffold version"
 
-    sh "skaffold build -f skaffold.yaml"
-    sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
+        sh "skaffold build -f skaffold.yaml"
+        sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
+    }
 }
