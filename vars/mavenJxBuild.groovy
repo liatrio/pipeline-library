f33264498f1ca751e30fc40b14575b1c84484dc5
Benjamin Stein <ben@liatrio.com>
2019-03-13 10:02:08 -0700

    put jx post step back in

--- vars/mavenJxBuild.groovy
+++ vars/mavenJxBuild.groovy
@@ -8,15 +8,15 @@ def call(params) {
             sh "mvn versions:set -DnewVersion=${appVersion}"
         } else {
             def pom = readMavenPom file: 'pom.xml'
-            appVersion = pom.version.split("-")[0] + env.BUILD_NUMBER
+            appVersion = pom.version.split("-")[0] + "-${BUILD_NUMBER}"
             // set environment variables
         }
         env.VERSION = appVersion
         sh "mvn clean install"
         sh "skaffold version"
-//        sh "jx step tag --version ${appVersion}"
+        sh "jx step tag --version ${appVersion}"
 
         sh "skaffold build -f skaffold.yaml"
-//        sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:${appVersion}"
+        sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
     }
 }
