2d2998d20bead0f500dea938825d3feb2d5d39a9
Benjamin Stein <ben@liatrio.com>
2019-03-13 13:44:26 -0700

    add timeRecovery method

---
+++ vars/getTimeItTookToRecover.groovy
@@ -0,0 +1,21 @@
+def call(params) {
+    def currentBuild = params.currentBuild
+    def completedTimeStamp = currentBuild.getTimeInMillis()
+
+    def prevTimeStamp = getTimeOfFailedBuild(currentBuild)
+    println "Timestamp of first failed build was: ${prevTimeStamp}"
+    return (completedTimeStamp -prevTimeStamp)/1000
+
+}
+
+
+
+@NonCPS
+def getTimeOfFailedBuild(currentBuild) {
+    def build = currentBuild //current build is fixed
+
+    while(build.id > 1 && build.getPreviousBuild().getResult() != 'SUCCESS') {
+        build = build.getPreviousBuild()
+    }
+    return build.getTimeInMillis()
+}
