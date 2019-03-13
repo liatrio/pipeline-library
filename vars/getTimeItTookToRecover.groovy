6a6eee16aaba3ff62006a8198a3adc3b70c095b0
Benjamin Stein <ben@liatrio.com>
2019-03-13 15:03:53 -0700

    set currentBuild data in method

--- vars/getTimeItTookToRecover.groovy
+++ vars/getTimeItTookToRecover.groovy
@@ -1,9 +1,9 @@
-def call(currentBuild, String unit = "MINUTES") {
+def call(currentBuild, String unit = "MILLISECONDS") {
 
     def divisor = ["HOURS": 360000, "MINUTES": 60000, "SECONDS": 1000 , "MILLISECONDS": 1]
     long completedTimeStamp = currentBuild.getTimeInMillis()
     long prevTimeStamp = getTimeOfFailedBuild(currentBuild)
-
+    currentBuild.recoveryTime = completedTimeStamp - prevTimeStamp
     return (completedTimeStamp - prevTimeStamp) / divisor[unit]
 }
 
