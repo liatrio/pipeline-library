eea966ede124f6dec790903d9c5b8865f9712b9a
Benjamin Stein <ben@liatrio.com>
2019-03-13 15:07:26 -0700

    set recovery time as an environment variable

--- vars/getTimeItTookToRecover.groovy
+++ vars/getTimeItTookToRecover.groovy
@@ -3,7 +3,7 @@ def call(currentBuild, String unit = "MILLISECONDS") {
     def divisor = ["HOURS": 360000, "MINUTES": 60000, "SECONDS": 1000 , "MILLISECONDS": 1]
     long completedTimeStamp = currentBuild.getTimeInMillis()
     long prevTimeStamp = getTimeOfFailedBuild(currentBuild)
-    currentBuild.recoveryTime = completedTimeStamp - prevTimeStamp
+    env.recoveryTime = completedTimeStamp - prevTimeStamp
     return (completedTimeStamp - prevTimeStamp) / divisor[unit]
 }
 
