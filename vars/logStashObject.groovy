4a72b4976bb0345aa34a5c1bccf2683a33a69a59
Benjamin Stein <ben@liatrio.com>
2019-03-13 16:15:12 -0700

    convert groovyMap to json string

--- vars/logStashObject.groovy
+++ vars/logStashObject.groovy
@@ -1,11 +1,11 @@
 def call (params) {
 
-    def requestBody = [
-            "team-name": 'team stein',
+    def requestBody = """
+       {     "team-name": 'team stein',
             "app-name" : 'appStein',
             "branch": 'master',
-            "TTR": params.recoveryTime
-    ]
+            "TTR": params.recoveryTime }
+    """
     def url = env.elasticUrl
     def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestBody, url: url
     println('Status: ' + response.status)
