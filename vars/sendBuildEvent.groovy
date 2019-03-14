44d262c23647cc2e3776cb0d7ea8ea1f2279ba79
Benjamin Stein <ben@liatrio.com>
2019-03-14 12:27:01 -0700

    use jsonOutput to convert groovy map to json

--- vars/sendBuildEvent.groovy
+++ vars/sendBuildEvent.groovy
@@ -1,4 +1,4 @@
-def call (requestBody) {
+def call (requestParams) {
 /*
     def requestBody = [
           "teamName": ${teamName},
@@ -9,12 +9,14 @@ def call (requestBody) {
           "priorDuration": 16343
     ]
  */
-    requestBody.teamName = env.TEAM_NAME ? env.TEAM_NAME : env.ORG
-    requestBody.appName = env.APP_NAME
-    requestBody.branch = env.BRANCH_NAME
+    requestParams.teamName = env.TEAM_NAME ? env.TEAM_NAME : env.ORG
+    requestParams.appName = env.APP_NAME
+    requestParams.branch = env.BRANCH_NAME
+
+    def requestBody = JsonOutput.toJson(requestParams)
     def url = env.elasticUrl ? env.elasticUrl : "localhost:9200"
 
-    def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestBody, url: url
+    def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestParams, url: url
     println('Status: ' + response.status)
     println('Response: ' + response.content)
 }
