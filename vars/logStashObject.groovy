00d61550460f42fc5cc48e540969a06e21c9b981
Benjamin Stein <ben@liatrio.com>
2019-03-14 10:32:29 -0700

    expanded template

--- vars/logStashObject.groovy
+++ vars/logStashObject.groovy
@@ -1,10 +1,13 @@
 def call (params) {
 
     def requestBody = """
-       {     "team-name": 'team stein',
+         {     
+            "team-name": 'team stein',
             "app-name" : 'appStein',
-            "branch": 'master',
-            "TTR": params.recoveryTime }
+            "TTR": params.recoveryTime,
+            "build": true,
+            "test": false
+         }
     """
     def url = env.elasticUrl
     def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestBody, url: url
