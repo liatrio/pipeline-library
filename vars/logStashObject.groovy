4a902ddd3e0894ab116528379856daceba98d49b
Benjamin Stein <ben@liatrio.com>
2019-03-14 11:20:54 -0700

    use new doc spec

--- vars/logStashObject.groovy
+++ vars/logStashObject.groovy
@@ -1,14 +1,18 @@
 def call (params) {
 
     def requestBody = """
-         {     
-            "team-name": 'team stein',
-            "app-name" : 'appStein',
-            "TTR": params.recoveryTime,
-            "build": true,
-            "test": false
-         }
+         {
+              "team-name": 'team stein',
+              "app-name" : 'appStein',
+              "event-name": “state-change”,
+               “branch”: “master”,
+               “state”: “healthy”,
+               “prior-duration”: 16343
+            }
+
     """
+
+    def index = 'build_event'
     def url = env.elasticUrl
     def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: requestBody, url: url
     println('Status: ' + response.status)
