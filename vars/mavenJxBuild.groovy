5a46c3ea74b822dc5a1d573bcabedbdffeecb33e
Benjamin Stein <ben@liatrio.com>
2019-03-14 16:34:17 -0700

    check file

--- vars/mavenJxBuild.groovy
+++ vars/mavenJxBuild.groovy
@@ -33,6 +33,7 @@ def call(params) {
                 sh "ls"
                 def environmentInfo = readFile 'previewEnvironment.txt'
                 println environmentInfo
+                println "boom ${environmentInfo[-1]}"
                 env.APP_URL = environmentInfo.readLines()[-1] - "Preview application is now available at: "
             }
             echo "url to sample app is: ${env.APP_URL}"
