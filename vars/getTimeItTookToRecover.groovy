bb0af1667c8dbdf40f3f8693d051678a4c0eba46
Benjamin Stein <stein321@gmail.com>
2019-03-14 10:06:29 -0700

    Eng 312 (#9)
    * add timeRecovery method
    
    * simplify parameters to just current build
    
    * formatting
    
    * debug
    
    * debug
    
    * use static types for number comparisons
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * use id method that returns an int instead of string
    
    * use id method that returns an int instead of string
    
    * get time back in minutes
    
    * unit type as parameter
    
    * set currentBuild data in method
    
    * set recovery time as an environment variable
    
    * debug
    
    * send data to logstash
    
    * send data to logstash
    
    * convert groovyMap to json string
    
    * for now, don't send info to es
    
    * post to elasticsearch

---
+++ vars/getTimeItTookToRecover.groovy
@@ -0,0 +1,22 @@
+def call(currentBuild, String unit = "MILLISECONDS") {
+
+    def divisor = ["HOURS": 360000, "MINUTES": 60000, "SECONDS": 1000 , "MILLISECONDS": 1]
+    long completedTimeStamp = currentBuild.getTimeInMillis()
+    long prevTimeStamp = getTimeOfFailedBuild(currentBuild)
+    recoveryTime = completedTimeStamp - prevTimeStamp
+    logStashObject(recoveryTime: recoveryTime  )
+    return (completedTimeStamp - prevTimeStamp) / divisor[unit]
+}
+
+
+
+@NonCPS
+long getTimeOfFailedBuild(currentBuild) {
+    def build = currentBuild //current build is fixed
+
+    while(build.getNumber() > 1 && build.getPreviousBuild().getResult() != 'SUCCESS') {
+        build = build.getPreviousBuild()
+    }
+    println "build that failed first ${build.getNumber()}"
+    return build.getTimeInMillis()
+}
