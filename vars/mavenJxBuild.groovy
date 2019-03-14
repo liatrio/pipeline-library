55ba04541d31bca64b4afd3fb87d8956eac7cc41
Benjamin Stein <ben@liatrio.com>
2019-03-14 15:47:37 -0700

    get url to sample app

--- vars/mavenJxBuild.groovy
+++ vars/mavenJxBuild.groovy
@@ -29,8 +29,10 @@ def call(params) {
         if (env.BRANCH_NAME.contains("PR")) {
             dir('charts/preview') {
                 sh "make preview"
-                sh "jx preview --app $APP_NAME --dir ../.."
+                sh "jx preview --app $APP_NAME --dir ../.. > previewEnvironment.txt"
+                env.APP_URL = new File('previewEnvironment.txt').readLines()[-1] - "Preview application is now available at: "
             }
         }
+        echo "url to sample app is: ${env.APP_URL}"
     }
 }
