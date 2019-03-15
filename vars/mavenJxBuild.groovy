6a3400c74d08e107afdf44ba7781124125015e29
Benjamin Stein <ben@liatrio.com>
2019-03-15 11:38:40 -0700

    get app name from pom file instead of statically defined in the jenkinsfile

--- vars/mavenJxBuild.groovy
+++ vars/mavenJxBuild.groovy
@@ -5,14 +5,15 @@ def call(params) {
     def appVersion = params.get("version", "")
     sendBuildEvent(eventType: 'build')
     container('maven') {
-
+        def pom = readMavenPom file: 'pom.xml'
         if (appVersion) {
             sh "mvn versions:set -DnewVersion=${appVersion}"
         } else {
-            def pom = readMavenPom file: 'pom.xml'
             appVersion = pom.version.split("-")[0] + "-${BUILD_NUMBER}"
         }
         env.VERSION = appVersion
+        env.APP_NAME = pom.artifactId
+
         sh "mvn clean install"
         sh "skaffold version"
 
