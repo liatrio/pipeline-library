0849e32c03ccfd2b03193311a287a9e93fda8c64
H607678 <jeffg@liatrio.com>
2019-04-08 17:55:03 -0700

    trying to use kubeConfig file

--- vars/deployToAks.groovy
+++ vars/deployToAks.groovy
@@ -1,7 +1,14 @@
 def call(env, config) {
 
-  sh "kubectl version"
+  withCredentials([file(credentialsId: 'kubeConfig', variable: 'kubeConfig')]) {
+    sh """
+      kubectl config --kubeconfig=${kubeConfig} use-context aksCluster
+      kubectl --kubeconfig=${kubeConfig} get pods
+    """
+  }
   //sh """
+  //  sh "kubectl config set-cluster ${env} --server=toolchain-jenkins-11073318.hcp.eastus.azmk8s.io"
+  //  sh "kubectl config --kubeconfig=config-demo set-credentials jenkins --client-certificate=${FAKE_CERT_FILE} --client-key=fake-key-seefile"
   //  kubectl config --kubeconfig=config-demo set-cluster ${env} --server=https://<aks_ip> --certificate-authority=<fake-ca-file>
   //  kubectl config --kubeconfig=config-demo set-context ${env} --cluster=<still needed> --namespace=storage --user=developer
   //  kubectl config --kubeconfig=config-demo use-context ${env}
