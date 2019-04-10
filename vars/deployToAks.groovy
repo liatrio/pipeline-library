c83a15de0795ef30b09ce404462737e896960e13
H607678 <jeffg@liatrio.com>
2019-04-10 09:55:29 -0700

    fixing name

--- vars/deployToAks.groovy
+++ vars/deployToAks.groovy
@@ -4,13 +4,15 @@ def call(env, helmName, helmChoice, helmValues) {
   withCredentials([file(credentialsId: 'kubeConfig', variable: 'kubeConfig')]) {
     def kubeCmd = "kubectl --kubeconfig=${kubeConfig}"
     def helmCmd = "helm --kubeconfig=${kubeConfig}"
+    def values = readFile file: "${helmValues}"
+    writeFile file: "values.yaml", text: values
 
     sh """
       ${kubeCmd} config use-context ${env}
       ${helmCmd} init
     """
     if (helmChoice == "install") {
-      sh "${helmCmd} install liatrio-jenkins/. --name ${helmName} -f ${helmValues}"
+      sh "${helmCmd} install liatrio-jenkins/. --name ${helmName}-jenkins -f values.yaml"
     }
     else if (helmChoice == "delete") {
       sh "${helmCmd} delete --purge ${helmName}"
