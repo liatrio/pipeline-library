d4d26d7d63b9c4c9776d3f59d96beb596956d8d4
Joe Knight <joe@devnill.com>
2019-04-11 14:41:07 -0500

    Testing openshift access

--- vars/mavenJxBuildArtifactory.groovy
+++ vars/mavenJxBuildArtifactory.groovy
@@ -28,17 +28,24 @@ def call(params) {
             openshift.withCluster("${OPENSHIFT_CLUSTER}") {
                 openshift.withProject("${OPENSHIFT_PROJECT}") {
                     echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
+
+                    // Create a Selector capable of selecting all service accounts in mycluster's default project
+                    def saSelector = openshift.selector( 'serviceaccount' )
+
+                    // Prints `oc describe serviceaccount` to Jenkins console
+                    saSelector.describe()
+
+                    // Selectors also allow you to easily iterate through all objects they currently select.
+                    saSelector.withEach { // The closure body will be executed once for each selected object.
+                        // The 'it' variable will be bound to a Selector which selects a single
+                        // object which is the focus of the iteration.
+                        echo "Service account: ${it.name()} is defined in ${openshift.project()}"
+                    }
+
                     docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
                         sh "skaffold build -p openshift-online -f skaffold.yaml"
                         //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
                     }
-                }
-            }
-        }
-        openshift.withCredentials('openshift-liatrio') {
-            openshift.withCluster("${OPENSHIFT_CLUSTER}") {
-                openshift.withProject('liatrio') {
-                    echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
                 }
             }
         }
