30ec8f920dc53ca8f7da4e54ab7a3042483128ee
H607678 <jeffg@liatrio.com>
2019-04-11 09:42:16 -0700

    updating deploy jenkins

--- vars/deployJenkins.groovy
+++ vars/deployJenkins.groovy
@@ -1,19 +1,20 @@
-def call(env, helmName, helmChoice) {
+def call(env, teamName, helmChoice) {
 
 
   withCredentials([file(credentialsId: 'kubeConfig', variable: 'kubeConfig')]) {
     def kubeCmd = "kubectl --kubeconfig=${kubeConfig}"
     def helmCmd = "helm --kubeconfig=${kubeConfig}"
 
+    git branch: 'master', url: 'https://github.com/liatrio/liatrio-jenkins.git'
     sh """
       ${kubeCmd} config use-context ${env}
       ${helmCmd} init
     """
     if (helmChoice == "install") {
-      sh "${helmCmd} install liatrio-jenkins/. --name ${helmName}-jenkins -f teams/${helmName}.yaml"
+      sh "${helmCmd} install liatrio-jenkins/. --name ${teamName}-jenkins --set teamName=${teamName} -f credentials.yaml -f jobs.yaml"
     }
     else if (helmChoice == "delete") {
-      sh "${helmCmd} delete --purge ${helmName}-jenkins"
+      sh "${helmCmd} delete --purge ${teamName}-jenkins"
     }
   }
 }
