8f391362f564745624da1d65e449e48686e3e85d
Joe Knight <joe@devnill.com>
2019-04-12 14:34:33 -0500

    Trying string token

--- vars/mavenJxBuildArtifactory.groovy
+++ vars/mavenJxBuildArtifactory.groovy
@@ -5,6 +5,47 @@ def call(params) {
     def appVersion = params.get("version", "")
     sendBuildEvent(eventType: 'build')
     container('maven') {
+
+        openshift.logLevel(10)
+        sh "cat ~/.kube/config || true"
+        withEnv(["PATH+OC=${tool 'oc3.11'}"]) {
+//            withCredentials([string(credentialsId: 'openshift-login-token', variable: 'TOKEN')]) {
+//                sh "oc login ${OPENSHIFT_CLUSTER} -t ${OPENSHIFT_TOKEN}"
+//            }
+            sh "cat ~/.kube/config || true"
+            withCredentials([string(credentialsId: 'openshift-login-token', variable: 'OC_TOKEN')]) {
+                openshift.withCluster("insecure://${OPENSHIFT_CLUSTER}", "${OC_TOKEN}") {
+                    openshift.withProject("${OPENSHIFT_PROJECT}") {
+                        sh "cat ~/.kube/config || true"
+                        openshift.logLevel(10)
+                        echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
+                        def result = openshift.raw('status', '-v')
+                        echo "Cluster status: ${result.out}"
+
+                        // Create a Selector capable of selecting all service accounts in mycluster's default project
+                        def saSelector = openshift.selector('serviceaccount')
+
+                        // Prints `oc describe serviceaccount` to Jenkins console
+                        saSelector.describe()
+
+                        // Selectors also allow you to easily iterate through all objects they currently select.
+                        saSelector.withEach {
+                            // The closure body will be executed once for each selected object.
+                            // The 'it' variable will be bound to a Selector which selects a single
+                            // object which is the focus of the iteration.
+                            echo "Service account: ${it.name()} is defined in ${openshift.project()}"
+                        }
+
+                        docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
+                            sh "skaffold diagnose  -f skaffold.yaml"
+                            sh "skaffold -v debug run -p openshift-online -f skaffold.yaml"
+                            //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
+                        }
+                    }
+                }
+            }
+        }
+
         def pom = readMavenPom file: 'pom.xml'
         if (appVersion) {
             sh "mvn versions:set -DnewVersion=${appVersion}"
@@ -23,44 +64,7 @@ def call(params) {
             sh "jx step git credentials"
             sh "jx step tag --version ${appVersion}"
         }
-        env.SKIP_TLS = true
-        openshift.logLevel(10)
-        sh "cat ~/.kube/config || true"
-        withEnv(["PATH+OC=${tool 'oc3.11'}"]) {
-//            withCredentials([string(credentialsId: 'openshift-login-token', variable: 'TOKEN')]) {
-//                sh "oc login ${OPENSHIFT_CLUSTER} -t ${OPENSHIFT_TOKEN}"
-//            }
-            sh "cat ~/.kube/config || true"
-            openshift.withCluster("insecure://${OPENSHIFT_CLUSTER}", '263df250-da49-4158-9cf6-22677ecb0120') {
-                openshift.withProject("${OPENSHIFT_PROJECT}") {
-                    sh "cat ~/.kube/config || true"
-                    openshift.logLevel(10)
-                    echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
-                    def result = openshift.raw( 'status', '-v' )
-                    echo "Cluster status: ${result.out}"
 
-                    // Create a Selector capable of selecting all service accounts in mycluster's default project
-                    def saSelector = openshift.selector('serviceaccount')
-
-                    // Prints `oc describe serviceaccount` to Jenkins console
-                    saSelector.describe()
-
-                    // Selectors also allow you to easily iterate through all objects they currently select.
-                    saSelector.withEach {
-                        // The closure body will be executed once for each selected object.
-                        // The 'it' variable will be bound to a Selector which selects a single
-                        // object which is the focus of the iteration.
-                        echo "Service account: ${it.name()} is defined in ${openshift.project()}"
-                    }
-
-                    docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
-                        sh "skaffold diagnose  -f skaffold.yaml"
-                        sh "skaffold -v debug run -p openshift-online -f skaffold.yaml"
-                        //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
-                    }
-                }
-            }
-        }
 //        if (env.BRANCH_NAME.contains("PR")) {
 //            dir('charts/preview') {
 //                sh "make preview"
