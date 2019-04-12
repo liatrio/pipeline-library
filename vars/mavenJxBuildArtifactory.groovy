75655fd5b1f2ed7850c2213f69bd8178be256a80
Joe Knight <joe@devnill.com>
2019-04-12 14:30:10 -0500

    Trying openshift oauth token

--- vars/mavenJxBuildArtifactory.groovy
+++ vars/mavenJxBuildArtifactory.groovy
@@ -31,34 +31,32 @@ def call(params) {
 //                sh "oc login ${OPENSHIFT_CLUSTER} -t ${OPENSHIFT_TOKEN}"
 //            }
             sh "cat ~/.kube/config || true"
-            openshift.withCluster("insecure://${OPENSHIFT_CLUSTER}") {
+            openshift.withCluster("insecure://${OPENSHIFT_CLUSTER}", '263df250-da49-4158-9cf6-22677ecb0120') {
                 openshift.withProject("${OPENSHIFT_PROJECT}") {
-                    openshift.withCredentials('openshift-login-login') {
-                        sh "cat ~/.kube/config || true"
-                        openshift.logLevel(10)
-                        echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
-                        def result = openshift.raw( 'status', '-v' )
-                        echo "Cluster status: ${result.out}"
+                    sh "cat ~/.kube/config || true"
+                    openshift.logLevel(10)
+                    echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
+                    def result = openshift.raw( 'status', '-v' )
+                    echo "Cluster status: ${result.out}"
 
-                        // Create a Selector capable of selecting all service accounts in mycluster's default project
-                        def saSelector = openshift.selector('serviceaccount')
+                    // Create a Selector capable of selecting all service accounts in mycluster's default project
+                    def saSelector = openshift.selector('serviceaccount')
 
-                        // Prints `oc describe serviceaccount` to Jenkins console
-                        saSelector.describe()
+                    // Prints `oc describe serviceaccount` to Jenkins console
+                    saSelector.describe()
 
-                        // Selectors also allow you to easily iterate through all objects they currently select.
-                        saSelector.withEach {
-                            // The closure body will be executed once for each selected object.
-                            // The 'it' variable will be bound to a Selector which selects a single
-                            // object which is the focus of the iteration.
-                            echo "Service account: ${it.name()} is defined in ${openshift.project()}"
-                        }
+                    // Selectors also allow you to easily iterate through all objects they currently select.
+                    saSelector.withEach {
+                        // The closure body will be executed once for each selected object.
+                        // The 'it' variable will be bound to a Selector which selects a single
+                        // object which is the focus of the iteration.
+                        echo "Service account: ${it.name()} is defined in ${openshift.project()}"
+                    }
 
-                        docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
-                            sh "skaffold diagnose  -f skaffold.yaml"
-                            sh "skaffold -v debug run -p openshift-online -f skaffold.yaml"
-                            //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
-                        }
+                    docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
+                        sh "skaffold diagnose  -f skaffold.yaml"
+                        sh "skaffold -v debug run -p openshift-online -f skaffold.yaml"
+                        //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
                     }
                 }
             }
