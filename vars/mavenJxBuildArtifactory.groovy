ede5134b0b5b9f8f7e3f6e9e89ed70ea127f33d1
Joe Knight <joe@devnill.com>
2019-04-12 17:02:59 -0500

    Removing extra debugging

--- vars/mavenJxBuildArtifactory.groovy
+++ vars/mavenJxBuildArtifactory.groovy
@@ -36,34 +36,12 @@ def call(params) {
 
                         sh "oc login https://${OPENSHIFT_CLUSTER} --token=${OC_TOKEN}"
 
-                        sh "cat ~/.kube/config || true"
-                        openshift.logLevel(10)
-                        echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
-
-                        result = openshift.raw('status', '-v')
-                        echo "Cluster status: ${result.out}"
-
-                        // Create a Selector capable of selecting all service accounts in mycluster's default project
-                        def saSelector = openshift.selector('serviceaccount')
-
-                        // Prints `oc describe serviceaccount` to Jenkins console
-                        saSelector.describe()
-
-                        // Selectors also allow you to easily iterate through all objects they currently select.
-                        saSelector.withEach {
-                            // The closure body will be executed once for each selected object.
-                            // The 'it' variable will be bound to a Selector which selects a single
-                            // object which is the focus of the iteration.
-                            echo "Service account: ${it.name()} is defined in ${openshift.project()}"
-                        }
-
                         result = openshift.raw('status', '-v')
                         echo "Cluster status: ${result.out}"
 
                         sh "helm --debug version"
                         docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
-                            sh "skaffold diagnose  -f skaffold.yaml"
-                            sh "skaffold -v debug run -p openshift-online -f skaffold.yaml"
+                            sh "skaffold build -p openshift-online -f skaffold.yaml"
                             //sh "jx step post build --image $DOCKER_REGISTRY/$ORG/$APP_NAME:$VERSION"
                         }
                     }
