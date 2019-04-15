5f6b45013668665932446233619704d9d51eb306
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-04-15 14:59:24 -0700

    updating skaffoldbuild

--- vars/skaffoldBuild.groovy
+++ vars/skaffoldBuild.groovy
@@ -1,12 +1,7 @@
 #!/bin/env groovy
 
 def call(params) {
-
-    if (sh "skaffold version") {
-        docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
-            sh "skaffold build -v debug -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
-        }
-    } else {
-        throw new Exception("No Skaffold Installed")
+    docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
+        sh "skaffold build -v debug -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
     }
 }
