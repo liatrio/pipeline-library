7646c6715085e98f15b0c5d267970193ffb6db16
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-04-15 16:49:00 -0700

    reverting back to old chartname

--- vars/deployHelmArtifactory.groovy
+++ vars/deployHelmArtifactory.groovy
@@ -5,7 +5,7 @@ def call(params) {
       sh """
         helm --debug init --client-only
         helm repo add helm "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
-        helm package --dependency-update --version ${env.VERSION} --app-version ${env.VERSION} sample-app-ui
+        helm package --dependency-update --version ${env.VERSION} --app-version ${env.VERSION} ${CHART_NAME}
         """
       rtServer (
         id: "liatrio-artifactory",
