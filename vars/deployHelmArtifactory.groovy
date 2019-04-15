fb2ace7071be1a42350f18381305862b12f20a60
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-04-15 16:41:33 -0700

    adding in helm init

--- vars/deployHelmArtifactory.groovy
+++ vars/deployHelmArtifactory.groovy
@@ -3,6 +3,7 @@
 def call(params) {
     withCredentials([usernamePassword(credentialsId: 'artifactory-takumin', variable: 'CREDS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
       sh """
+        helm --debug init --client-only
         helm repo add helm "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
         helm package --dependency-update --version ${env.VERSION} --app-version ${env.VERSION} ${CHART_PATH}
         """
