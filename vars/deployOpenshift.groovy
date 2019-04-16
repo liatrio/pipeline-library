6abb069f2df142f193512baba14c6c04bdeece1c
Chris Schreiber <chriss@liatrio.com>
2019-04-16 11:45:07 -0700

    turn off debug output

--- vars/deployOpenshift.groovy
+++ vars/deployOpenshift.groovy
@@ -15,15 +15,13 @@ def call(params) {
     if (!params) params = [:]
     container('maven') {
       withEnv(["PATH+OC=${tool 'oc3.11'}"]) {
-        sh "cat ~/.kube/config || true"
         withCredentials([string(credentialsId: 'openshift-login-token', variable: 'OC_TOKEN')]) {
           openshift.withCluster("insecure://${OPENSHIFT_CLUSTER}", "${OC_TOKEN}") {
             openshift.withProject("${OPENSHIFT_PROJECT}") {
               sh "oc login https://${OPENSHIFT_CLUSTER} --token=${OC_TOKEN}"
               result = openshift.raw('status', '-v')
               echo "Cluster status: ${result.out}"
-              sh "helm --debug version"
-              sh "helm --debug init --client-only"
+              sh "helm init --client-only"
               withCredentials([usernamePassword(credentialsId: 'artifactory-takumin', variable: 'CREDS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                 sh """
                   helm repo add liatrio-artifactory "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
--- vars/skaffoldBuild.groovy
+++ vars/skaffoldBuild.groovy
@@ -2,6 +2,6 @@
 
 def call(params) {
     docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
-        sh "skaffold build -v debug -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
+        sh "skaffold build -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
     }
 }
