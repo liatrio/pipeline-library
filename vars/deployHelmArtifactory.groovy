43aa2045550acb6313224e2b2abdda80ba7140d3
Chris Schreiber <chriss@liatrio.com>
2019-04-16 17:51:28 -0700

    fix indent

--- vars/deployHelmArtifactory.groovy
+++ vars/deployHelmArtifactory.groovy
@@ -5,18 +5,18 @@
  * PARAMETERS
  *   helmRepository
  *   helmRepositoryCredentials
- */
+ **/
 def call(params) {
   if (!params) params = [:]
   sh "helm init --client-only"
   sh "helm package --dependency-update --version ${VERSION} --app-version ${VERSION} ${CHART_PATH}"
   rtServer (
-    id: "liatrio-artifactory",
-    url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/helm"),
-    credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
-    )
-    rtUpload (
-      serverId: "liatrio-artifactory",
-      specPath: "${APP_NAME}-${VERSION}.tgz"
-      )
-    }
+     id: "liatrio-artifactory",
+     url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/helm"),
+     credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
+  )
+  rtUpload (
+    serverId: "liatrio-artifactory",
+    specPath: "${APP_NAME}-${VERSION}.tgz"
+  )
+}
