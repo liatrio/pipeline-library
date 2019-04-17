aa4a401f7e8df7bec5960113c761cead2f82fe09
Chris Schreiber <chriss@liatrio.com>
2019-04-16 17:49:28 -0700

    add helm repo params

--- vars/deployHelmArtifactory.groovy
+++ vars/deployHelmArtifactory.groovy
@@ -1,28 +1,22 @@
 #!/bin/env groovy
-
+/**
+ * Publish a Helm chart to an Artifactory repository
+ *
+ * PARAMETERS
+ *   helmRepository
+ *   helmRepositoryCredentials
+ */
 def call(params) {
-    withCredentials([usernamePassword(credentialsId: 'artifactory-takumin', variable: 'CREDS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-      sh """
-        helm init --client-only
-        #helm repo add helm "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
-        helm package --dependency-update --version ${env.VERSION} --app-version ${env.VERSION} ${CHART_PATH}
-        """
-      rtServer (
-        id: "liatrio-artifactory",
-        url: "http://artifactory.liatr.io/artifactory",
-        credentialsId: "artifactory-takumin"
+  if (!params) params = [:]
+  sh "helm init --client-only"
+  sh "helm package --dependency-update --version ${VERSION} --app-version ${VERSION} ${CHART_PATH}"
+  rtServer (
+    id: "liatrio-artifactory",
+    url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/helm"),
+    credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
+    )
+    rtUpload (
+      serverId: "liatrio-artifactory",
+      specPath: "${APP_NAME}-${VERSION}.tgz"
       )
-      rtUpload (
-        serverId: "liatrio-artifactory",
-        spec:
-          """{
-             "files": [
-              {
-                "pattern": "${APP_NAME}-${env.VERSION}.tgz",
-                "target": "helm/"
-               }
-            ]
-          }"""
-      )
-   }
-}
+    }
