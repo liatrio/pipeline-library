90d5f72c4a46aac2441540cc1dd15a540ff008a0
Chris Schreiber <chriss@liatrio.com>
2019-04-16 17:28:36 -0700

    disable helm init

--- vars/deployHelmArtifactory.groovy
+++ vars/deployHelmArtifactory.groovy
@@ -3,8 +3,8 @@
 def call(params) {
     withCredentials([usernamePassword(credentialsId: 'artifactory-takumin', variable: 'CREDS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
       sh """
-        helm init --client-only
-        helm repo add helm "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
+        #helm init --client-only
+        #helm repo add helm "https://artifactory.liatr.io/artifactory/helm" --username $USERNAME --password $PASSWORD
         helm package --dependency-update --version ${env.VERSION} --app-version ${env.VERSION} ${CHART_PATH}
         """
       rtServer (
