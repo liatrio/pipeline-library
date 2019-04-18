5c4b39027e5092f47e8a36d0075ac178a45a0be2
Jonathon Rudy <31257900+JonRudy@users.noreply.github.com>
2019-04-18 15:32:05 -0700

    Update publishHelmChartArtifactory.groovy

--- vars/publishHelmChartArtifactory.groovy
+++ vars/publishHelmChartArtifactory.groovy
@@ -12,7 +12,7 @@ def call(params) {
   sh "helm package --dependency-update --version ${VERSION} --app-version ${VERSION} ${CHART_PATH}"
   rtServer (
      id: "liatrio-artifactory",
-     url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/"),
+     url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/helm"),
      credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
   )
   rtUpload (
