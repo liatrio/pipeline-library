bad2d438b1e3256fa7470f98102298049b57b3d3
Takumi Nishida <takumin@liatrio.com>
2019-04-18 15:37:16 -0700

    removing echos

--- vars/publishHelmChartArtifactory.groovy
+++ vars/publishHelmChartArtifactory.groovy
@@ -10,13 +10,11 @@ def call(params) {
   if (!params) params = [:]
   sh "helm init --client-only"
   sh "helm package --dependency-update --version ${VERSION} --app-version ${VERSION} ${CHART_PATH}"
-  sh "echo 'before rtserver'"
   rtServer (
      id: "liatrio-artifactory",
      url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/"),
      credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
   )
-  sh "echo 'after rtserver'"
   rtUpload (
     serverId: "liatrio-artifactory",
     spec:
