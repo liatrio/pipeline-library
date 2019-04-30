29ede3c0412b812665b20e06e905b3001aa13618
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-04-30 10:19:48 -0700

    updating get params

--- vars/skaffoldBuild.groovy
+++ vars/skaffoldBuild.groovy
@@ -2,7 +2,7 @@
 
 def call(params) {
   if (!params) params = [:]
-  docker.withRegistry("https://${DOCKER_REGISTRY}", '${params.get("artifactoryCredentials", "artifactory-credentials")}') {
+  docker.withRegistry("https://${DOCKER_REGISTRY}", params.get("artifactoryCredentials", "artifactory-credentials")) {
     sh "skaffold build -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
   }
 }
