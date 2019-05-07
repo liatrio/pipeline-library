c99beba2bfbbfe75e219168ad3d6291ade1d1bed
Chris Schreiber <chriss@liatrio.com>
2019-05-07 15:30:29 -0700

    get docker credential from param / change default cred name

--- vars/skaffoldBuild.groovy
+++ vars/skaffoldBuild.groovy
@@ -1,7 +1,9 @@
 #!/bin/env groovy
 
-def call(params) {
-    docker.withRegistry("https://${DOCKER_REGISTRY}", 'artifactory-takumin') {
+def call(params)
+    if (!params) params = [:]
+
+    docker.withRegistry("https://${DOCKER_REGISTRY}", params.get("artifactoryCredential", 'jenkins-credential-artifactory') {
         sh "skaffold build -p ${SKAFFOLD_PROFILE} -f skaffold.yaml"
     }
 }
