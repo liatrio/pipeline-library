f0b90ed22d6f98a562ddac1d7bc64ab0ee1f5347
Jonathon Rudy <31257900+JonRudy@users.noreply.github.com>
2019-05-07 15:37:16 -0700

    Updating deploy artifactory (#19)
    * adding in new way to push artifactory
    
    * commenting out pushing
    
    * adding in deploy to artifactory
    
    * removing unused code
    
    * making changes to use parmas
    
    * adding in null check to prevent failures when params set but env vars not set

--- vars/publishHelmChartArtifactory.groovy
+++ vars/publishHelmChartArtifactory.groovy
@@ -2,41 +2,28 @@
 /**
  * Publish a Helm chart to an Artifactory repository
  *
- * ENVIRONMENT VARIABLES
- *   VERSION: chart version
- *
  * PARAMETERS
- *   helmRepository: DEFAULT = "https://artifactory.liatr.io/artifactory/"
- *   helmRepositoryCredentials: DEFAULT = "openshift-token"
+ *   helmRepository: DEFAULT = "https://artifactory.liatr.io/artifactory/helm"
  *   chartPath: DEFAULT = "charts/"
  *   chartName: DEFAULT = APP_NAME
  *   repos: List of repos to add to helm for chart dependencies
+ *   version: Used for chart version - DEFAULT = VERSION.
  **/
 def call(params) {
   if (!params) params = [:]
   def chartPath = params.get("chartPath", "charts/")
-  def chartName = params.get("chartName", APP_NAME)
+  def chartName = params.get("chartName", null)
+  if (!chartName) chartName = APP_NAME
+  def helmRepository = params.get("helmRepository", "https://artifactory.liatr.io/artifactory/helm")
+  def version = params.get("version", null)
+  if (!version) version = VERSION
   sh "helm init --client-only"
   def repos = params.get("repos")
   repos?.each {
     sh "helm repo add ${it.name} ${it.url}"
   }
-  sh "helm package --dependency-update --version ${VERSION} --app-version ${VERSION} ${chartPath}/${chartName}"
-  rtServer (
-     id: "liatrio-artifactory",
-     url: params.get("helmRepository", "https://artifactory.liatr.io/artifactory/"),
-     credentialsId: params.get("helmRepositoryCredentials", "openshift-token")
-  )
-  rtUpload (
-    serverId: "liatrio-artifactory",
-    spec:
-      """{
-        "files": [
-          {
-            "pattern": "${chartName}-${VERSION}.tgz",
-            "target": "helm/"
-          }
-        ]
-     }"""
-  )
+  sh "helm package --dependency-update --version ${version} --app-version ${version} ${chartPath}/${chartName}"
+  withCredentials([usernamePassword(credentialsId: 'jenkins-credential-artifactory', passwordVariable: 'artifactoryPassword', usernameVariable: 'artifactoryUsername')]) {
+    sh "curl -X PUT -u ${env.artifactoryUsername}:${env.artifactoryPassword} -T ${chartName}-${version}.tgz '${helmRepository}/${chartName}-${version}.tgz'"
+  }
 }
